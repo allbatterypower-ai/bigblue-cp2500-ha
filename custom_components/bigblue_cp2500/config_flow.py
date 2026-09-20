b556f976c959884fea37dfa6f69dfef05b913ccf
allbatterypower-ai <allbatterypower@gmail.com>
2026-09-20 21:04:15 +0300

    Add GitHub upload options

--- custom_components/bigblue_cp2500/config_flow.py
+++ custom_components/bigblue_cp2500/config_flow.py
@@ -6,6 +6,8 @@ from homeassistant.const import CONF_ADDRESS
 
 from .const import DOMAIN, DEFAULT_ADDRESS
 
+DEFAULT_GITHUB_REPO = "allbatterypower-ai/bigblue-cp2500-logs"
+
 
 class BigBlueConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     VERSION = 1
@@ -24,5 +26,38 @@ class BigBlueConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
             step_id="user",
             data_schema=vol.Schema(
                 {vol.Required(CONF_ADDRESS, default=DEFAULT_ADDRESS): str}
+            ),
+        )
+
+    @staticmethod
+    def async_get_options_flow(config_entry):
+        return BigBlueOptionsFlow(config_entry)
+
+
+class BigBlueOptionsFlow(config_entries.OptionsFlow):
+    def __init__(self, config_entry):
+        self.config_entry = config_entry
+
+    async def async_step_init(self, user_input=None):
+        if user_input is not None:
+            return self.async_create_entry(title="", data=user_input)
+
+        return self.async_show_form(
+            step_id="init",
+            data_schema=vol.Schema(
+                {
+                    vol.Optional(
+                        "github_repo",
+                        default=self.config_entry.options.get(
+                            "github_repo", DEFAULT_GITHUB_REPO
+                        ),
+                    ): str,
+                    vol.Optional(
+                        "github_token",
+                        default=self.config_entry.options.get(
+                            "github_token", ""
+                        ),
+                    ): str,
+                }
             ),
         )
