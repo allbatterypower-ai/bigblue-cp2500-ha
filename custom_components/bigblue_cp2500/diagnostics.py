40356940760d7194aa587adaf21cdfedc54631da
allbatterypower-ai <allbatterypower@gmail.com>
2026-09-20 21:16:58 +0300

    Update diagnostics version

--- custom_components/bigblue_cp2500/diagnostics.py
+++ custom_components/bigblue_cp2500/diagnostics.py
@@ -13,7 +13,7 @@ async def async_get_config_entry_diagnostics(
     coordinator = hass.data[DOMAIN][entry.entry_id]
 
     return {
-        "integration_version": "0.3.2",
+        "integration_version": "0.3.5",
         "parsed_data": coordinator.data,
         "raw_main_frame_hex": (
             coordinator.last_raw_main_frame.hex(" ")
